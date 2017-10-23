0bd75ca88f2a821090399541a9d080ae5e2616fc
gabrielalewandowska <gabriela.l.lewandowska@gmail.com>
2017-10-23 16:45:37 +0100

    can retrieve all films in json

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var Films = require("./client/src/models/films");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('client/build'));
 
-
+app.get("/api/films", function(req, res){
+  res.json(Films());
+});
 
 
 
 
 
 app.listen(3000, function () {
-  console.log('App running on port '+this.address().port);
+  console.log('App running on port ' + this.address().port);
 });
---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+config = {
+    entry: __dirname + "/client/src/app.js",
+    output: {
+        filename: "bundle.js",
+        path: __dirname + "/public"
+    }
+}
+
+module.exports = config;
